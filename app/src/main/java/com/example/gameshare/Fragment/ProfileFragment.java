684c402e53ecadf250c8fd896c8a3b24627fa1c7
Faisal Rahman <faisal_rahman@live.com>
2019-03-30 00:08:37 +0600

    Search post,card view

--- app/src/main/java/com/example/gameshare/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/gameshare/Fragment/ProfileFragment.java
@@ -1,26 +1,46 @@
 package com.example.gameshare.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.gameshare.R;
-
+import com.example.gameshare.StartActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProfileFragment extends Fragment {
 
+    Button logout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        logout  = view.findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(  getContext(), StartActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        return view;
     }
+
+
 
 
 }
--- app/src/main/java/com/example/gameshare/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/gameshare/Fragment/SearchFragment.java
@@ -4,21 +4,72 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.gameshare.Adapter.PostAdapter;
+import com.example.gameshare.Adapter.SearchAdapter;
+import com.example.gameshare.Model.Post;
 import com.example.gameshare.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerViewS;
+    private SearchAdapter postAdapter;
+    private List<Post> postList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerViewS = view.findViewById(R.id.recyclerViewS);
+        recyclerViewS.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+        recyclerViewS.setLayoutManager(mLayoutManager);
+        postList = new ArrayList<>();
+        postAdapter = new SearchAdapter(getContext(), postList);
+        recyclerViewS.setAdapter(postAdapter);
+        readPosts();
+        return view;
+    }
+    private void readPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    postList.add(post);
+
+                }
+
+                postAdapter.notifyDataSetChanged();
+//                progress_circular.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
